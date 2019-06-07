5541e5f7f1991b57a8f0a6885e67f3b7c147fb40
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-06-07 18:32:32 -0400

    feat: adds outputs for examples

--- examples/standard/README.md
+++ examples/standard/README.md
@@ -20,4 +20,21 @@ Note that this example may create resources which can cost money (AWS Elastic IP
 | access\_key | Credentials: AWS access key. | string | n/a | yes |
 | secret\_key | Credentials: AWS secret key. Pass this as a variable, never write password in the code. | string | n/a | yes |
 
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| efs\_arn |  |
+| efs\_dns\_name |  |
+| efs\_id |  |
+| efs\_mount\_target\_dns\_names |  |
+| efs\_mount\_target\_ids |  |
+| efs\_mount\_target\_network\_interface\_ids |  |
+| kms\_alias\_arn |  |
+| kms\_key\_arn |  |
+| kms\_key\_id |  |
+| security\_group\_id |  |
+| ssm\_parameter\_arns |  |
+| ssm\_parameter\_names |  |
+
 <!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
--- examples/standard/outputs.tf
+++ examples/standard/outputs.tf
@@ -1,1 +1,63 @@
+#####
+# EFS
+#####
 
+output "efs_arn" {
+  value = "${module.standard.efs_arn}"
+}
+
+output "efs_id" {
+  value = "${module.standard.efs_id}"
+}
+
+output "efs_dns_name" {
+  value = "${module.standard.efs_dns_name}"
+}
+
+output "efs_mount_target_network_interface_ids" {
+  value = "${module.standard.efs_mount_target_network_interface_ids}"
+}
+
+output "efs_mount_target_ids" {
+  value = "${module.standard.efs_mount_target_ids}"
+}
+
+output "efs_mount_target_dns_names" {
+  value = "${module.standard.efs_mount_target_dns_names}"
+}
+
+#####
+# KMS
+#####
+
+output "kms_key_id" {
+  value = "${module.standard.kms_key_id}"
+}
+
+output "kms_key_arn" {
+  value = "${module.standard.kms_key_arn}"
+}
+
+output "kms_alias_arn" {
+  value = "${module.standard.kms_alias_arn}"
+}
+
+#####
+# SSM Parameter
+#####
+
+output "ssm_parameter_arns" {
+  value = "${module.standard.ssm_parameter_arns}"
+}
+
+output "ssm_parameter_names" {
+  value = "${module.standard.ssm_parameter_names}"
+}
+
+#####
+# Security group
+#####
+
+output "security_group_id" {
+  value = "${module.standard.security_group_id}"
+}
