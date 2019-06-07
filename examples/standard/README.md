e2a26968449c1a8f608b52bfef945630b9a8d1d9
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-06-07 16:27:01 -0400

    test: adds standard test

---
+++ examples/standard/README.md
@@ -0,0 +1,23 @@
+# EFS module standard test
+
+## Usage
+
+To run this example you need to execute:
+
+```bash
+$ terraform init
+$ terraform plan
+$ terraform apply
+```
+
+Note that this example may create resources which can cost money (AWS Elastic IP, for example). Run `terraform destroy` when you don't need these resources.
+
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| access\_key | Credentials: AWS access key. | string | n/a | yes |
+| secret\_key | Credentials: AWS secret key. Pass this as a variable, never write password in the code. | string | n/a | yes |
+
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
---
+++ examples/standard/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  version    = "~> 2.2.0"
+  region     = "eu-west-1"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+
+resource "random_string" "this" {
+  length  = 8
+  upper   = false
+  special = false
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+module "standard" {
+  source = "../../"
+
+  tags = {
+    Name = "tftest"
+  }
+
+  subnet_ids_count = "${length(data.aws_subnet_ids.default.ids)}"
+  subnet_ids       = "${data.aws_subnet_ids.default.ids}"
+
+  name = "tftest${random_string.this.result}"
+
+  kms_key_alias_name = "/alias/tftest/tftest${random_string.this.result}"
+  kms_key_name       = "/alias/tftest/tftest${random_string.this.result}"
+
+  ssm_parameter_enabled = true
+  ssm_parameter_prefix  = "/ssm/tftest${random_string.this.result}"
+}
