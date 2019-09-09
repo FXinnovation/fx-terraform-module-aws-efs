d2096d4847690990e92c3a7ecc5e859f01af54e9
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-09-09 14:11:10 -0400

    Upgrade to terraform 0.12

--- main.tf
+++ main.tf
@@ -3,30 +3,42 @@
 #####
 
 resource "aws_efs_file_system" "this" {
-  count = "${var.enabled ? 1 : 0}"
+  count = var.enabled ? 1 : 0
 
   encrypted  = true
-  kms_key_id = "${var.kms_key_create ? element(concat(aws_kms_key.this.*.arn, list("")), 0) : var.kms_key_arn}"
+  kms_key_id = var.kms_key_create ? element(concat(aws_kms_key.this.*.arn, [""]), 0) : var.kms_key_arn
 
-  provisioned_throughput_in_mibps = "${var.provisioned_throughput_in_mibps}"
-  performance_mode                = "${var.performance_mode}"
-  throughput_mode                 = "${var.throughput_mode}"
+  provisioned_throughput_in_mibps = var.provisioned_throughput_in_mibps
+  performance_mode                = var.performance_mode
+  throughput_mode                 = var.throughput_mode
 
-  tags = "${merge(
-    map("Terraform", "true"),
-    map("Name", var.name),
+  tags = merge(
+    {
+      "Terraform" = "true"
+    },
+    {
+      "Name" = var.name
+    },
     var.tags,
-    var.efs_tags
-  )}"
+    var.efs_tags,
+  )
 }
 
 resource "aws_efs_mount_target" "this" {
-  count = "${var.enabled && var.subnet_ids_count > 0 ? var.subnet_ids_count : 0}"
+  count = var.enabled && var.subnet_ids_count > 0 ? var.subnet_ids_count : 0
 
-  file_system_id = "${aws_efs_file_system.this.id}"
-  subnet_id      = "${element(var.subnet_ids, count.index)}"
+  file_system_id = aws_efs_file_system.this[0].id
+  subnet_id      = element(var.subnet_ids, count.index)
 
-  security_groups = ["${var.security_group_create ? element(concat(aws_security_group.this.*.id, list("")), 0) : join(",", var.security_group_ids)}"]
+  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+  # force an interpolation expression to be interpreted as a list by wrapping it
+  # in an extra set of list brackets. That form was supported for compatibility in
+  # v0.11, but is no longer supported in Terraform v0.12.
+  #
+  # If the expression in the following list itself returns a list, remove the
+  # brackets to avoid interpretation as a list of lists. If the expression
+  # returns a single list item then leave it as-is and remove this TODO comment.
+  security_groups = [var.security_group_create ? element(concat(aws_security_group.this.*.id, [""]), 0) : join(",", var.security_group_ids)]
 }
 
 #####
@@ -34,23 +46,27 @@ resource "aws_efs_mount_target" "this" {
 #####
 
 resource "aws_kms_key" "this" {
-  count = "${var.enabled && var.kms_key_create ? 1 : 0}"
+  count = var.enabled && var.kms_key_create ? 1 : 0
 
   description = "KMS Key for ${var.name} EFS encryption."
 
-  tags = "${merge(
-    map("Terraform", "true"),
-    map("Name", var.kms_key_name),
+  tags = merge(
+    {
+      "Terraform" = "true"
+    },
+    {
+      "Name" = var.kms_key_name
+    },
     var.tags,
-    var.kms_tags
-  )}"
+    var.kms_tags,
+  )
 }
 
 resource "aws_kms_alias" "this" {
-  count = "${var.enabled && var.kms_key_create ? 1 : 0}"
+  count = var.enabled && var.kms_key_create ? 1 : 0
 
-  name          = "${var.kms_key_alias_name}"
-  target_key_id = "${aws_kms_key.this.key_id}"
+  name          = var.kms_key_alias_name
+  target_key_id = aws_kms_key.this[0].key_id
 }
 
 #####
@@ -58,19 +74,21 @@ resource "aws_kms_alias" "this" {
 #####
 
 resource "aws_ssm_parameter" "this_efs_id" {
-  count = "${var.enabled && var.ssm_parameter_enabled ? 1 : 0}"
+  count = var.enabled && var.ssm_parameter_enabled ? 1 : 0
 
   name        = "${var.ssm_parameter_prefix}/efs/id"
   description = "ID of the ${var.name} EFS."
   type        = "String"
-  value       = "${aws_efs_file_system.this.id}"
+  value       = aws_efs_file_system.this[0].id
   overwrite   = true
 
-  tags = "${merge(
-    map("Terraform", "true"),
+  tags = merge(
+    {
+      "Terraform" = "true"
+    },
     var.tags,
     var.ssm_parameter_tags,
-  )}"
+  )
 }
 
 #####
@@ -78,29 +96,41 @@ resource "aws_ssm_parameter" "this_efs_id" {
 #####
 
 resource "aws_security_group" "this" {
-  count = "${var.enabled && var.security_group_create ? 1 : 0}"
+  count = var.enabled && var.security_group_create ? 1 : 0
 
-  name        = "${var.security_group_name}"
+  name        = var.security_group_name
   description = "Security group for ${var.name} EFS."
-  vpc_id      = "${local.vpc_id}"
+  vpc_id      = local.vpc_id
 
-  tags = "${merge(
-    map("Terraform", "true"),
-    map("Name", var.security_group_name),
+  tags = merge(
+    {
+      "Terraform" = "true"
+    },
+    {
+      "Name" = var.security_group_name
+    },
     var.tags,
     var.security_group_tags,
-  )}"
+  )
 }
 
 resource "aws_security_group_rule" "this" {
-  count = "${var.enabled && var.security_group_create ? length(var.security_group_allowed_cidrs) : 0}"
+  count = var.enabled && var.security_group_create ? length(var.security_group_allowed_cidrs) : 0
 
-  security_group_id = "${element(concat(aws_security_group.this.*.id, list("")), 0)}"
+  security_group_id = element(concat(aws_security_group.this.*.id, [""]), 0)
 
   type        = "ingress"
   from_port   = 2049
   to_port     = 2049
   protocol    = "tcp"
-  description = "NFS from ${lookup(var.security_group_allowed_cidrs[count.index], "target")}."
-  cidr_blocks = ["${lookup(var.security_group_allowed_cidrs[count.index], "cidr")}"]
+  description = "NFS from ${var.security_group_allowed_cidrs[count.index]["target"]}."
+  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+  # force an interpolation expression to be interpreted as a list by wrapping it
+  # in an extra set of list brackets. That form was supported for compatibility in
+  # v0.11, but is no longer supported in Terraform v0.12.
+  #
+  # If the expression in the following list itself returns a list, remove the
+  # brackets to avoid interpretation as a list of lists. If the expression
+  # returns a single list item then leave it as-is and remove this TODO comment.
+  cidr_blocks = [var.security_group_allowed_cidrs[count.index]["cidr"]]
 }
--- outputs.tf
+++ outputs.tf
@@ -4,27 +4,27 @@
 
 output "efs_arn" {
   description = "Amazon Resource Name of the file system."
-  value       = "${element(concat(aws_efs_file_system.this.*.arn, list("")), 0)}"
+  value       = element(concat(aws_efs_file_system.this.*.arn, [""]), 0)
 }
 
 output "efs_id" {
   description = "The ID that identifies the file system (e.g. fs-ccfc0d65)."
-  value       = "${element(concat(aws_efs_file_system.this.*.id, list("")), 0)}"
+  value       = element(concat(aws_efs_file_system.this.*.id, [""]), 0)
 }
 
 output "efs_dns_name" {
   description = "The DNS name for the filesystem."
-  value       = "${element(concat(aws_efs_file_system.this.*.dns_name, list("")), 0)}"
+  value       = element(concat(aws_efs_file_system.this.*.dns_name, [""]), 0)
 }
 
 output "efs_mount_target_network_interface_ids" {
   description = "The IDs of the network interfaces that Amazon EFS created when it created the mount targets."
-  value       = "${aws_efs_mount_target.this.*.network_interface_id}"
+  value       = aws_efs_mount_target.this.*.network_interface_id
 }
 
 output "efs_mount_target_ids" {
   description = "The IDs of the EFS mount targets."
-  value       = "${aws_efs_mount_target.this.*.id}"
+  value       = aws_efs_mount_target.this.*.id
 }
 
 #####
@@ -33,17 +33,17 @@ output "efs_mount_target_ids" {
 
 output "kms_key_id" {
   description = "The globally unique identifier for the EFS key. This output will be empty if the KMS key was passed as variable."
-  value       = "${element(concat(aws_kms_key.this.*.id, list("")), 0)}"
+  value       = element(concat(aws_kms_key.this.*.id, [""]), 0)
 }
 
 output "kms_key_arn" {
   description = "The Amazon Resource Name (ARN) of the EFS key. This output will be empty if the KMS key was passed as variable."
-  value       = "${element(concat(aws_kms_key.this.*.arn, list("")), 0)}"
+  value       = element(concat(aws_kms_key.this.*.arn, [""]), 0)
 }
 
 output "kms_alias_arn" {
   description = "The Amazon Resource Name (ARN) of the EFS key alias. This output will be empty if the KMS key was passed as variable."
-  value       = "${element(concat(aws_kms_alias.this.*.arn, list("")), 0)}"
+  value       = element(concat(aws_kms_alias.this.*.arn, [""]), 0)
 }
 
 #####
@@ -52,12 +52,20 @@ output "kms_alias_arn" {
 
 output "ssm_parameter_arns" {
   description = "The ARNs of the SSM Parameters for the EFS."
-  value       = "${compact(list(element(concat(aws_ssm_parameter.this_efs_id.*.arn, list("")), 0)))}"
+  value = compact(
+    [
+      element(concat(aws_ssm_parameter.this_efs_id.*.arn, [""]), 0),
+    ],
+  )
 }
 
 output "ssm_parameter_names" {
   description = "The names of the SSM Parameters for the EFS."
-  value       = "${compact(list(element(concat(aws_ssm_parameter.this_efs_id.*.name, list("")), 0)))}"
+  value = compact(
+    [
+      element(concat(aws_ssm_parameter.this_efs_id.*.name, [""]), 0),
+    ],
+  )
 }
 
 #####
@@ -66,5 +74,5 @@ output "ssm_parameter_names" {
 
 output "security_group_id" {
   description = "ID of the security group used for the EFS. This output will be empty if the security groups IDs were passed as variables."
-  value       = "${element(concat(aws_security_group.this.*.id, list("")), 0)}"
+  value       = element(concat(aws_security_group.this.*.id, [""]), 0)
 }
