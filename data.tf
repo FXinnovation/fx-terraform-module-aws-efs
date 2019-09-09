83b7ed38d2a5304f1a2e5991945f9aa65578f616
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-09-09 14:27:59 -0400

    Add workaround

--- data.tf
+++ data.tf
@@ -6,8 +6,15 @@ data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
 
+# NOTE: This is a workaround https://github.com/terraform-providers/terraform-provider-aws/issues/7522
+locals {
+  subnet_ids_string = join(",", data.aws_subnet_ids.default.ids)
+  subnet_ids_list   = split(",", local.subnet_ids_string)
+}
+
+
 data "aws_subnet" "default" {
-  id = element(concat(var.subnet_ids, [""]), 0) != "" ? element(concat(var.subnet_ids, [""]), 0) : element(concat(data.aws_subnet_ids.default.ids, [""]), 0)
+  id = element(concat(var.subnet_ids, [""]), 0) != "" ? element(concat(var.subnet_ids, [""]), 0) : element(concat(local.subnet_ids_list, [""]), 0)
 }
 
 locals {
