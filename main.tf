84e78003ed2cbec7c82798147af58c57788e3cee
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-09-10 10:09:47 -0400

    Fixing 0.12 upgrade command malformation

--- main.tf
+++ main.tf
@@ -30,15 +30,7 @@ resource "aws_efs_mount_target" "this" {
   file_system_id = aws_efs_file_system.this[0].id
   subnet_id      = element(var.subnet_ids, count.index)
 
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibility in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
-  security_groups = [var.security_group_create ? element(concat(aws_security_group.this.*.id, [""]), 0) : join(",", var.security_group_ids)]
+  security_groups = var.security_group_create ? list(element(concat(aws_security_group.this.*.id, [""]), 0)) : var.security_group_ids
 }
 
 #####
